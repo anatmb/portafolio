8b44adadfcf04b267bbbd79dfd890dfdcaf61c37
anatmb <anatmb@hotmail.com>
2024-05-09 20:25:18 -0300

    hover de project

--- src/components/content/content.jsx
+++ src/components/content/content.jsx
@@ -7,34 +7,64 @@ function content() {
 
     const navigate = useNavigate();
 
-  const redirectToServicesPage = () => {
-    navigate('/services'); // Reemplaza '/services' con la ruta a tu página de servicios
-  };
+    const redirectToServicesPage = () => {
+        navigate('/services'); // Reemplaza '/services' con la ruta a tu página de servicios
+    };
 
-  const redirectToContactPage = () => {
-    navigate('/contact'); // Reemplaza '/contact' con la ruta a tu página de contacto
-  };
+    const redirectToContactPage = () => {
+        navigate('/contact'); // Reemplaza '/contact' con la ruta a tu página de contacto
+    };
 
     return (
-<main className="container-content">
-    <div className='row'>
-        <div className='col-lg-6 left-content my-5'>
-            <h1 className='title-content'>
-                Construyendo el Futuro Digital: Desarrollador Frontend
-            </h1>
-            <p className='paragraph-content'>
-                Como desarrollador frontend, mi pasión radica en convertir ideas en experiencias digitales cautivadoras y funcionales. Con un enfoque en la creación de interfaces de usuario atractivas y fáciles de usar.
-            </p>
-            <div className="d-flex">
-                <button className="btn custom-btn-primary px-4 mr-2" onClick={redirectToServicesPage}>Servicios</button>
-                <button className="btn custom-btn-secondary px-4" onClick={redirectToContactPage}>Contáctame</button>
+        <main className="container-content">
+            <div className='row'>
+                <div className='col-lg-6 left-content'>
+                    <h1 className='title-content'>
+                        Anadeska Meléndez
+                    </h1>
+
+                    <h2 className='d-flex justify-content-left align-items-left'><span className='paragraph-nombre'> Desarrollador Frontend </span></h2>
+                    <p className='paragraph-content'>
+                        Como desarrollador frontend, mi pasión radica en convertir ideas en experiencias digitales cautivadoras y funcionales. Con un enfoque en la creación de interfaces de usuario atractivas y fáciles de usar.
+                    </p>
+
+                    <div className='iconos'>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" height="40" alt="javascript logo" />
+                        <img width="12" />
+                        <img width="12" />
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" height="40" alt="react logo" />
+                        <img width="12" />
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" height="40" alt="nodejs logo" />
+                        <img width="12" />
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg" height="40" alt="angularjs logo" />
+                        <img width="12" />
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" height="40" alt="linkedin logo" />
+                        <img width="12" />
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" height="40" alt="redux logo" />
+                        <img width="12" />
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" height="40" alt="express logo" />
+                        <img width="12" />
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" height="40" alt="postgresql logo" />
+                        <img width="12" />
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-original.svg" height="40" alt="sequelize logo" />
+                        <img width="12" />
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" height="40" alt="html5 logo" />
+                        <img width="12" />
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" height="40" alt="css3 logo" />
+                        <img width="12" />
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" height="40" alt="bootstrap logo" />
+
+                    </div>
+                    <div className="d-flex my-5">
+                        <button className="btn custom-btn-primary px-4 mr-2" onClick={redirectToServicesPage}>Servicios</button>
+                        <button className="btn custom-btn-secondary px-4" onClick={redirectToContactPage}>Contáctame</button>
+                    </div>
+                </div>
+                <div className='col-lg-6 right-main d-flex justify-content-center align-items-center'>
+                    <img className="photo-content img-fluid" src={imagen} alt="" />
+                </div>
             </div>
-        </div>
-        <div className='col-lg-6 right-main d-flex justify-content-center align-items-center'>
-            <img className="photo-content img-fluid" src={imagen} alt="" />
-        </div>
-    </div>
-</main>
+        </main>
     )
 }
 
--- src/components/page/projects.jsx
+++ src/components/page/projects.jsx
@@ -9,36 +9,56 @@ import imagen4 from '../../assets/project-04.png';
 function projects() {
   return (
     <div className='colorr'>
-        <div class="album py-5 bg-body-tertiary">
-    <div class="container">
+      <div class="album py-5 bg-body-tertiary">
+        <div class="container">
 
-      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-     
-        <div class="col">
-          <div class="card shadow-sm">
-          {/* <img src={imagen} alt="" /> */}
-          < NavLink to="#"><img src={imagen} className="img-fluid border project-image" /></NavLink>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card shadow-sm">
-          < NavLink to="#"><img src={imagen2} className="img-fluid border project-image" /></NavLink>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card shadow-sm">
-          < NavLink to="#"><img src={imagen3} className="img-fluid border project-image" /></NavLink>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card shadow-sm">
-          < NavLink to="#"><img src={imagen4} className="img-fluid border project-image" /></NavLink>
-          </div>
-        </div> 
+          <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
 
+            <div class="col card-container">
+              <div class="card shadow-sm">
+                {/* <img src={imagen} alt="" /> */}
+                < NavLink to="#"><img src={imagen} className="img-fluid border project-image" /></NavLink>
+                <div className='card_hover'>
+                  <p>Nombre</p>
+                  <NavLink><button className='boton1'><i className="fa-solid fa-eye"></i></button></NavLink>
+                </div>
+              </div>
+            </div>
+
+            <div class="col card-container">
+              <div class="card shadow-sm">
+                < NavLink to="#"><img src={imagen2} className="img-fluid border project-image" /></NavLink>
+                <div className='card_hover'>
+                  <p>Nombre</p>
+                  <NavLink><button className='boton1'><i className="fa-solid fa-eye"></i></button></NavLink>
+                </div>
+              </div>
+            </div>
+
+
+            <div class="col card-container">
+              <div class="card shadow-sm">
+              < NavLink to="#"><img src={imagen3} className="img-fluid border project-image" /></NavLink>
+                <div className='card_hover'>
+                  <p>Nombre</p>
+                  <NavLink><button className='boton1'><i className="fa-solid fa-eye"></i></button></NavLink>
+                </div>
+              </div>
+            </div>
+
+            <div class="col card-container">
+              <div class="card shadow-sm">
+              < NavLink to="#"><img src={imagen4} className="img-fluid border project-image" /></NavLink>
+                <div className='card_hover'>
+                  <p>Nombre</p>
+                  <NavLink><button className='boton1'><i className="fa-solid fa-eye"></i></button></NavLink>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
     </div>
   )
 }
