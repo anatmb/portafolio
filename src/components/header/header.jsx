33015407a51f2fc9564e8987093a65016b2d7e66
anatmb <anatmb@hotmail.com>
2024-05-09 11:20:26 -0300

    to

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -7,23 +7,23 @@ function Header() {
         <header className="container-header container-fluid px-0 py-0 m-0">
             <nav className="navbar navbar-expand-lg">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">Portafolio</a>
+                    <a className="navbar-brand" to="/">Portafolio</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <NavLink className="nav-link active" aria-current="page" href="/about">Acerca</NavLink>
+                                <NavLink className="nav-link active" aria-current="page" to="/about">Acerca</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link" href="/projects">Proyectos</NavLink>
+    <NavLink className="nav-link" activeClassName="active" to="/projects">Proyectos</NavLink>
+</li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/services">Servicios</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link" href="/services">Servicios</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" href="/contact">Contáctame</NavLink>
+                                <NavLink className="nav-link" to="/contact">Contáctame</NavLink>
                             </li>
                         </ul>
                     </div>
