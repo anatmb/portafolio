ce051a59428e4309b282841929a2cce1e1715cc6
anatmb <anatmb@hotmail.com>
2024-05-10 17:24:58 -0300

    sin img

--- src/components/content/content.jsx
+++ src/components/content/content.jsx
@@ -28,30 +28,30 @@ function content() {
                         Como desarrollador frontend, mi pasión radica en convertir ideas en experiencias digitales cautivadoras y funcionales. Con un enfoque en la creación de interfaces de usuario atractivas y fáciles de usar.
                     </p>
 
-                    <div className='iconos'>
+                    <div className='iconos d-flex justify-content-left align-items-left '>
                         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" height="40" alt="javascript logo" />
-                        <img width="12" />
-                        <img width="12" />
+                       
+                       
                         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" height="40" alt="react logo" />
-                        <img width="12" />
+                       
                         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" height="40" alt="nodejs logo" />
-                        <img width="12" />
+                       
                         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg" height="40" alt="angularjs logo" />
-                        <img width="12" />
+                       
                         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" height="40" alt="linkedin logo" />
-                        <img width="12" />
+                       
                         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" height="40" alt="redux logo" />
-                        <img width="12" />
+                       
                         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" height="40" alt="express logo" />
-                        <img width="12" />
+                       
                         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" height="40" alt="postgresql logo" />
-                        <img width="12" />
+                       
                         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-original.svg" height="40" alt="sequelize logo" />
-                        <img width="12" />
+                       
                         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" height="40" alt="html5 logo" />
-                        <img width="12" />
+                       
                         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" height="40" alt="css3 logo" />
-                        <img width="12" />
+                       
                         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" height="40" alt="bootstrap logo" />
 
                     </div>
