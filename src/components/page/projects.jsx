21624466246f7816ebe60b4321a8714a76b7597f
anatmb <anatmb@hotmail.com>
2024-12-30 16:10:46 -0300

    Hexalamp3d

--- src/components/page/projects.jsx
+++ src/components/page/projects.jsx
@@ -5,6 +5,7 @@ import imagen from '../../assets/project-01.png';
 import imagen2 from '../../assets/project-02.png';
 import imagen3 from '../../assets/project-03.png';
 import imagen4 from '../../assets/project-04.png';
+import imagen5 from '../../assets/project-05.png';
 import { useNavigate } from 'react-router-dom';
 
 function projects() {
@@ -73,6 +74,19 @@ function projects() {
             </div>
             <h3 className='project-titule py-2'>Aplicación con acceso api TheDogApi</h3>
             <p className='project-subtitule'>React, redux, Bootstrap, HTML, CSS, Sequelize, PostgreSQL</p>
+            </div>
+          </div>
+
+          <div className="col mb-3">
+            <div className="card-container g-3">
+              <div className="card shadow-sm">
+              <NavLink to="https://impresion3-d.vercel.app/"><img src={imagen5} className="img-fluid border project-image" alt="Project 5" /></NavLink>
+              <div className='card_hover'>
+                <NavLink to="https://impresion3-d.vercel.app/"><button className='boton1'><i className="fa fa-external-link" aria-hidden="true"></i></button></NavLink>
+              </div>
+            </div>
+            <h3 className='project-titule py-2'>Landing page Hexalamp3D</h3>
+            <p className='project-subtitule'>HTML, CSS, JavaScript</p>
             </div>
           </div>
         </div>
